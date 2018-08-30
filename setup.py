20ce764edcc4f355e49211f20bd82e0a3b0b4b40
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2018-08-30 09:56:16 +0200

    add levmar library and parallel gauss_fit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
-from setuptools import setup
-
+from setuptools import setup, Extension
+import numpy
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -9,6 +9,14 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
+levmar_sources = [
+        os.path.join('cam_server/pipeline/data_processing/levmar-2.6', c)
+        for c in ['lm.c', 'Axb.c', 'misc.c', 'lmlec.c', 'lmbc.c', 'lmblec.c', 'lmbleic.c']
+        ]
+levmar_sources += [
+        'cam_server/pipeline/data_processing/levmar_c.c'
+]
 
 setup(name="cam_server",
       version="2.4.2",
@@ -20,6 +28,14 @@ setup(name="cam_server",
 
       license="GPL3",
 
+      ext_modules =[
+          Extension('cam_server.pipeline.data_processing.levmar_c',
+                sources=levmar_sources,
+		include_dirs = [numpy.get_include()],
+                extra_compile_args=['-std=c99', '-Wno-unused-but-set-variable', '-funroll-loops', '-fopenmp', '-march=native', '-O3'],
+                libraries=['gomp', 'mkl_rt']
+                )
+          ],
       packages=['cam_server',
                 "cam_server.camera",
                 "cam_server.camera.rest_api",
